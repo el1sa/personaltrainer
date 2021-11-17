5f07ad108ac0a76358d7365e098b91d01e49d55c
el1sa <elisa.tuovila@myy.haaga-helia.fi>
2021-11-17 15:02:58 +0200

    add ja delete toiminnot lisätty customerlistiin
    edit toiminto lisätty ja yritys add/delete training

---
+++ src/components/AddTraining.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+
+function AddTraining(props) {
+  const [open, setOpen] = React.useState(false);
+
+  const [training, setTraining] = React.useState({
+        date: '',
+        duration: '',
+        activity: '',
+        customer: props.customer 
+    })
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleSave = () => {
+      props.addTraining(training);
+      handleClose();
+  }
+
+  const inputChanged = e => {
+      setTraining({...training, [e.target.name]: e.target.value })
+  }
+
+
+  const inputDateChanged = e => {
+    setTraining({...training, date: new Date(e.target.value).toISOString().substring(0, 10) })
+}
+  
+    return (
+        <div>
+        <Button variant="contained" onClick={handleClickOpen}>
+        Add Training
+        </Button>
+        <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Add Training</DialogTitle>
+        <DialogContent>
+
+        <TextField
+            margin="dense"
+            name="date"
+            value={training.date}
+            onChange= {inputDateChanged}
+            label="Date"        
+            fullWidth
+            variant="standard"
+          />
+        
+          <TextField
+            margin="dense"
+            name="duration"
+            value={training.duration}
+            onChange={inputChanged}
+            label="Duration"        
+            fullWidth
+            variant="standard"
+          />
+          <TextField
+            margin="dense"
+            name="activity"
+            value={training.activity}
+            onChange={inputChanged}
+            label="Activity"        
+            fullWidth
+            variant="standard"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleSave}>Save</Button>
+        </DialogActions>
+        </Dialog>
+        </div>
+    );
+}
+
+export default AddTraining;
--- src/components/Customerlist.js
+++ src/components/Customerlist.js
@@ -1,17 +1,98 @@
 import React, { useState, useEffect } from 'react';
-import {AgGridReact} from 'ag-grid-react';
+import {AgGridReact} from 'ag-grid-react/lib/agGridReact';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-material.css';
+import AddCustomer from './AddCustomer';
+import { Snackbar } from '@mui/material';
+import Button from '@mui/material/Button';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditCustomer from './EditCustomer';
+import AddTraining from './AddTraining';
+
 
 function Customerlist() {
     const [customers, setCustomers] = useState([]);
 
+    const [trainings, setTrainings] = useState([]);
+
+    const [open, setOpen] = React.useState(false);
+
+    const [msg, setMsg] = useState('');
+
+    const handleClose = () => {
+        setOpen(false);
+      };    
+
     useEffect(() => {
+        fetchCustomers();
+     }, []);
+
+    const fetchCustomers = () => {
         fetch('https://customerrest.herokuapp.com/api/customers')
         .then(response => response.json())
         .then(data => setCustomers(data.content))
         .catch(err => console.error(err))
-    }, []);
+    }
+
+    const fetchTrainings = () => {
+        fetch('https://customerrest.herokuapp.com/api/trainings')
+        .then(response => response.json())
+        .then(data => setTrainings(data.content))
+        .catch(err => console.error(err))
+    }
+
+    const addCustomer = customer => {
+        fetch('https://customerrest.herokuapp.com/api/customers',
+        {
+            method: 'POST',
+            headers: {'Content-type' : 'application/json'},
+            body: JSON.stringify(customer)
+        })
+        .then(_ => fetchCustomers())
+        .catch(err => console.error(err))
+    }
+
+    const deleteCustomer = url => {
+        if (window.confirm('Sure you want to delete?')) {
+            fetch(url, {method: 'DELETE'})
+            .then(response => { 
+        if(response.ok) {
+            fetchCustomers()
+            setMsg("Customer deleted");
+            setOpen(true)
+        }
+        else
+            alert('Failed to delete')
+        })
+            .catch(err => console.error(err))
+        }
+    }
+
+    const editCustomer = (url, updateCustomer) => {
+        fetch(url, {
+            method: 'PUT',
+            headers: {'Content-type' : 'application/json'},
+            body: JSON.stringify(updateCustomer)
+        })
+        .then(_ => {
+            setMsg("Customer updated");
+            setOpen(true);
+            fetchCustomers()
+        })
+        .catch(err => console.log(err))
+    }
+
+    const addTraining = training => {
+        console.log(training)
+        fetch('https://customerrest.herokuapp.com/api/trainings',
+        {
+            method: 'POST',
+            headers: {'Content-type' : 'application/json'},
+            body: JSON.stringify(training)
+        })
+        .then(_ => fetchTrainings())
+        .catch(err => console.error(err))
+    }
 
     const columns = [
         {field: 'firstname', sortable: true, filter: true},
@@ -20,20 +101,53 @@ function Customerlist() {
         {field: 'postcode', sortable: true, filter: true},
         {field: 'city', sortable: true, filter: true, width: 120},
         {field: 'email', sortable: true, filter: true},
-        {field: 'phone', sortable: true, filter: true}
+        {field: 'phone', sortable: true, filter: true},
+        {
+            headerName: '',
+            sortable: false, 
+            filter: false,
+            width: 120,
+            field: 'links.0.href', 
+            cellRendererFramework: params => <Button variant="outlined" startIcon={<DeleteIcon />} size="small" color="error" onClick={() => deleteCustomer(params.value)}>Delete</Button>
+        },
+        {
+            headerName: '',
+            sortable: false, 
+            filter: false,
+            width: 120,
+            field: 'links.0.href',
+            cellRendererFramework: params => <EditCustomer editCustomer={editCustomer} customer={params} />
+        },
+        {
+            headerName: '',
+            sortable: false, 
+            filter: false,
+            width: 120,
+            field: 'links.0.href',
+            cellRendererFramework: params => <AddTraining addTraining={addTraining} customer={params.value} />
+        }
         
     ]
 
     
     return(
+        <div>
+            <AddCustomer addCustomer={addCustomer}/>
         <div className="ag-theme-material" style={{marginTop: 20, height: 600, width: '80%', margin: 'auto'}}>
-        <AgGridReact 
-            rowData={customers}
-            columnDefs={columns}
-            pagination={true}
-            paginationPageSize={10}
-            suppressCellSelection={true}
-        />
+            <AgGridReact 
+                rowData={customers}
+                columnDefs={columns}
+                pagination={true}
+                paginationPageSize={10}
+                suppressCellSelection={true}
+            />
+            <Snackbar 
+                open={open}
+                message={msg}
+                autoHideDuration={3000}
+                onClose={handleClose}
+            />
+        </div>
         </div>
     );
 }
